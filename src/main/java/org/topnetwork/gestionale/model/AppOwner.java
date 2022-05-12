1de4d44c1a718698c143ef94a386a2c84ae35663
mowgli <mathias.mazzei@icloud.com>
2022-05-12 14:42:32 +0200

    lombok impostato su tutte le classi scusate tutti sti push ma sono down
    *descrizione ad effetto*

--- src/main/java/org/topnetwork/gestionale/model/AppOwner.java
+++ src/main/java/org/topnetwork/gestionale/model/AppOwner.java
@@ -1,5 +1,8 @@
 package org.topnetwork.gestionale.model;
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class AppOwner {
-
+	private String pippo;
 }
--- src/main/java/org/topnetwork/gestionale/model/Rescan.java
+++ src/main/java/org/topnetwork/gestionale/model/Rescan.java
@@ -1,5 +1,8 @@
 package org.topnetwork.gestionale.model;
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class Rescan {
-
+	private String pippo;
 }
