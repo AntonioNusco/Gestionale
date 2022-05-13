ba318afc4300491c46f8c74d0708e95f74680c54
mowgli <mathias.mazzei@icloud.com>
2022-05-13 15:24:29 +0200

    attributi classe Application

--- src/main/java/org/topnetwork/gestionale/model/Application.java
+++ src/main/java/org/topnetwork/gestionale/model/Application.java
@@ -1,9 +1,23 @@
 package org.topnetwork.gestionale.model;
 
+import java.sql.Date;
+import java.sql.Time;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
 @Setter
+@AllArgsConstructor
 public class Application {
-	private String jesooo;
+	private int idApplicazione;
+	private int nodoConsole;
+	private Time avgAnalysisTime, launchingMeetingDataGatheringStarting;
+	private Date automationEnablingDate, insertedInCastProgram, stakeholderEngagement,
+	stakeholderBrief, onBoardingKitDelivery, primaRestitution;
+	private boolean done;
+	private String nomeApp, apmCode, ownerOnboarding, ownerAFP, gdsUnit, tecnologia, serverManager,
+	soloCMS, macchina, noteOnboarding, fase, afpStatus, pubblicatoDashboard, noteAppOwner, jiraAutomationAcitvision,
+	repoAvailability, automationStatus, automationNotes, greenItIndex, onboardingKitClosing, sourceCodeFinalDelivery,
+	linkConfluence, businessCritically, devMethodology, provider;
 }
