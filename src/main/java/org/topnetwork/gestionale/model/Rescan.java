059e5b7e592d749a4bbf549bde9f28999909cca8
mowgli <mathias.mazzei@icloud.com>
2022-05-16 09:58:51 +0200

    Update Rescan.java

--- src/main/java/org/topnetwork/gestionale/model/Rescan.java
+++ src/main/java/org/topnetwork/gestionale/model/Rescan.java
@@ -5,6 +5,15 @@ import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 
@@ -15,11 +24,23 @@ import lombok.Setter;
 
 public class Rescan {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idRescan;
+	@Column
 	private int nRescan,newOb,py,ytd;
+	@Column
 	private String aefpe,yoyRolling,lastRescan;
+	@Column
 	private boolean pnGoing,archive;
+	@Column
 	private Date rkd;
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_idApplicazione", referencedColumnName = "idApplicazione")
+	private Applicazione applicazione;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_idAppOwner", referencedColumnName = "idAppOwner")
+	private AppOwner appowner;
 	
 	
 	
