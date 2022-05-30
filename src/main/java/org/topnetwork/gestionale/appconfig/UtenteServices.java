ced31bd7a977edc6ab401bd45bda0766fc811273
SasiPiro <sasifpirozzi@gmail.com>
2022-05-30 12:23:59 +0200

    CORSFilter

--- src/main/java/org/topnetwork/gestionale/appconfig/UtenteServices.java
+++ src/main/java/org/topnetwork/gestionale/appconfig/UtenteServices.java
@@ -4,14 +4,19 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import static org.topnetwork.gestionale.utility.Utils.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import org.topnetwork.gestionale.dao.jpa.JpaDaoFactory;
+import org.topnetwork.gestionale.dao.jpa.JpaUtenteDao;
 import org.topnetwork.gestionale.model.Utente;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,6 +26,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class UtenteServices {
 
 	List<Utente> utenti = new ArrayList<Utente>();
+	
+	@Context
+	private HttpServletRequest httpRequest;
 
 	@GET
 	@Path("lista")
@@ -34,33 +42,7 @@ public class UtenteServices {
 			return null;
 		}
 	}
-
-//	@POST
-//	@Path("add")
-//	public boolean add(Utente u) {
-//		return JpaDaoFactory.getDaoFactory().save(u);
-//	}
-//}
-
-//	@POST
-//	@Path("add")
-//	public Response add(String s) throws URISyntaxException {
-//		ObjectMapper om = new ObjectMapper();
-//		Utente u = null;
-//		try {
-//			u = om.readValue(s,Utente.class);
-//		} catch (JsonMappingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (JsonProcessingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		JpaDaoFactory.getDaoFactory().save(u);
-//		return Response.created(new URI("api/utente/" + u.getIdUtente())).build();
-//	}
-	
-	
+		
 	@POST
 	@Path("add")
 	public Response add(Utente u) throws URISyntaxException {
@@ -69,7 +51,15 @@ public class UtenteServices {
 	}
 	
 	
-	
+	@POST
+	@Path("login")
+	public Response login(String email,String passw) throws URISyntaxException {
+		HttpSession session = httpRequest.getSession();
+			Utente u = JpaUtenteDao.getInstance().login(email, passw);
+			session.setAttribute("utenteloggato", u);
+			if(u.isRuolo()) return Response.created(new URI(null)).build();
+			return Response.created(new URI(null)).build();
+	}
 	
 	
 	
