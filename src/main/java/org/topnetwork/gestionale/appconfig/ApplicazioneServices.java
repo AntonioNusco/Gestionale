d9df0ba346c8ec7bb6f9b7fbf9b4fbb0afe5acb0
AntonioNusco <nuscoantonio1@gmail.com>
2022-05-30 16:22:07 +0200

    Bobbi Firmeniiii

--- src/main/java/org/topnetwork/gestionale/appconfig/ApplicazioneServices.java
+++ src/main/java/org/topnetwork/gestionale/appconfig/ApplicazioneServices.java
@@ -2,11 +2,15 @@ package org.topnetwork.gestionale.appconfig;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
+import org.json.JSONObject;
 import org.topnetwork.gestionale.dao.jpa.JpaDaoFactory;
 import org.topnetwork.gestionale.model.Applicazione;
 
@@ -19,15 +23,11 @@ public class ApplicazioneServices {
 	@GET
 	@Path("lista")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String listaApplicazioni() {
+	public Response listaApplicazioni() throws JsonProcessingException {
 		List<Applicazione> applicazioni = JpaDaoFactory.getDaoFactory().queryList(new Applicazione());
-		ObjectMapper om = new ObjectMapper();
-		try {
-			return om.writeValueAsString(applicazioni);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-			return null;
-		}
+		JSONObject json = new JSONObject();
+		json.put("applicazioni", applicazioni);
+		return Response.status(Response.Status.OK).entity(json.toString()).build();
 
 	}
 	
