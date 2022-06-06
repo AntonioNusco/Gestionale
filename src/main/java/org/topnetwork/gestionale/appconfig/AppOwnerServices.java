0b58aac2f8b6a90e90e69e76c0a89ed474af558b
AntonioNusco <nuscoantonio1@gmail.com>
2022-06-06 17:35:37 +0200

    bobbi

--- src/main/java/org/topnetwork/gestionale/appconfig/AppOwnerServices.java
+++ src/main/java/org/topnetwork/gestionale/appconfig/AppOwnerServices.java
@@ -14,6 +14,7 @@ import org.topnetwork.gestionale.model.AppOwner;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Path("appowner")
 public class AppOwnerServices {
 
 	@GET
